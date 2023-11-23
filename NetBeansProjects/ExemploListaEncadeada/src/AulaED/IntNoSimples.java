dc69b21b3cfb6cc5bf8e20e1741670c579fd3092
Lucas Monaco <102195525+Monaacoo@users.noreply.github.com>
2023-11-23 16:16:36 -0300

    Add files via upload

---
+++ NetBeansProjects/ExemploListaEncadeada/src/AulaED/IntNoSimples.java
@@ -0,0 +1,15 @@
+package AulaED;
+
+/**
+ *
+ * @author 0031432312007
+ */
+public class IntNoSimples {
+    int valor;
+    IntNoSimples prox;
+    
+    IntNoSimples(int ValorNo){
+        valor = ValorNo;
+        prox = null;
+    }
+}
